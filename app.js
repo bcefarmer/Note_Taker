5c25c5d9d483859e62aec811fdf0ad8f2eaef09c
Bruce Farmer <71850014+bcefarmer@users.noreply.github.com>
2021-01-23 16:58:53 -0500

    Api post

--- app.js
+++ app.js
@@ -1,10 +1,57 @@
 const express = require("express");
+const fs = require("fs");
+const path = require("path");
 const app = express();
 const PORT = 8000;
 
 
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
+// GET -----------------------
+
+app.get("/api/notes", function(req,res){
+  let noteData =  JSON.parse(fs.readFileSync(path.join(__dirname, "db", "db.json"), 'utf8'));
+   res.json(noteData);
+
+})    
+
+app.get("/notes", function(req,res){
+    res.sendFile(path.join(__dirname, "public", "notes.html"));
+})
+
+
+app.get("*", function(req,res){
+    res.sendFile(path.join(__dirname,"public", "index.html"));
+})
+
+// POST --------------------
+
+app.post("/api/notes", function(req,res){
+let dataStore = path.join(__dirname,"db", "db.json")
+let noteContent = req.body;
+fs.dataStore.append(res)
+
+
+    
+})
+
+
+
+
+
+// LISTENER
 
 app.listen(PORT, function(){
     console.log("App listening on PORT " + PORT);
 })
+    
+    
+   
+
+
+
+
+
+
+
