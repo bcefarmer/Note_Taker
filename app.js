08e8632dc5421556b377be1ce3391c47dce90e0e
Bruce Farmer <71850014+bcefarmer@users.noreply.github.com>
2021-01-25 22:08:08 -0500

    added uuid package

--- app.js
+++ app.js
@@ -1,53 +1,27 @@
+// EXPRESS
 const express = require("express");
 const app = express();
+const PORT = 8000;
 
+
+// Filepaths & writing to files
 const path = require("path");
 const fs = require("fs");
 
-const PORT = 8000;
-const bodyParser = require("body-parser");
 
+// Request parsing 
+const bodyParser = require("body-parser");
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(bodyParser.json());
 
-require("./public/returnFiles")(app);
-
-// GET -----------------------
-
-app.get("/api/notes", function(req,res){
-  let noteData =  JSON.parse(fs.readFileSync(path.join(__dirname, "db", "db.json"), 'utf8'));
-   res.json(noteData);
-
-})    
-// POST --------------------
-
-
-app.post("/api/notes", function(req,res){
-let dataStore = path.join(__dirname, "db", "db.json");
-
-
-
-let noteContent = req.body;
-
-
-
-dataStore.push(noteContent);
-
-
-fs.writeFile(db_Path, JSON.stringify(dataStore),(err) => {
-    if (err) {
-      console.log(err); 
-    } 
-    else { 
-      console.log("functioned");
-    }
-  });
-});
+// Point to jQuery HTML
+app.use(express.static(path.join(__dirname, "public")));
+require("./lib/returnFiles")(app);
+require("./lib/returnApi")(app);
 
 
 // LISTENER
-
 app.listen(PORT, function(){
     console.log("App listening on PORT " + PORT);
 })
